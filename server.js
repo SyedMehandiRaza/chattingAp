cf7c09ee80e7ae41cbc8f2932d87eb8b857cc818
SyedMehandiRaza <mehandixaidi786@gmail.com>
2025-12-24 11:56:10 +0530

    fix production cookie settings for railway

--- server.js
+++ server.js
@@ -9,11 +9,12 @@ socketHandler(server);
 console.log("🚀 Production log test");
 log('wshfff')
 
-const PORT = process.env.PORT || 2121;
 console.log("DB HOST:", process.env.P_DB_HOST);
 console.log("DB NAME:", process.env.P_DB_NAME);
 
 
-server.listen(PORT, () => {
-    log(`Server is running at Port: ${PORT}`)
-})
+const PORT = process.env.PORT || 8080;
+
+server.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
